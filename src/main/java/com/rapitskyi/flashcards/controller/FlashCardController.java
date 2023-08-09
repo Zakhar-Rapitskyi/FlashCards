b4ec6359edcf297ae01af4fd5a1f3a73f5f8b92b
Zakhar <limouoy@gmail.com>
2023-08-09 17:58:12 +0300

    add a delete button

--- src/main/java/com/rapitskyi/flashcards/controller/FlashCardController.java
+++ src/main/java/com/rapitskyi/flashcards/controller/FlashCardController.java
@@ -4,9 +4,12 @@ import com.rapitskyi.flashcards.entity.FlashCard;
 import com.rapitskyi.flashcards.repository.FlashCardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,10 +22,14 @@ public class FlashCardController {
 
     @GetMapping("/")
     public String hello(Model model) {
-        List<FlashCard> flashCardList = flashCardRepository.findAll(Sort.by(Sort.Direction.ASC,"word"));
-        model.addAttribute("name", "Zakhar");
+        List<FlashCard> flashCardList = flashCardRepository.findAll(Sort.by(Sort.Direction.ASC, "word"));
         model.addAttribute("flashCardsList", flashCardList);
-        model.addAttribute("flashCardsListSize", flashCardList.size());
         return "welcome";
+    }
+
+    @DeleteMapping("/deleteWord/{id}")
+    public ResponseEntity<?> deleteWord(@PathVariable("id") Long id) {
+        flashCardRepository.deleteById(id);
+        return ResponseEntity.ok("");
     }
 }
